ca298e3399c4f52013d0ff1b81c4574b77cd7c57
Alex Ott <alexott@gmail.com>
2009-06-26 17:14:56 +0200

    commit forgotten file

---
+++ rc/emacs-rc-common-hooks.el
@@ -0,0 +1,43 @@
+;;; emacs-rc-common-hooks.el ---
+
+;; Copyright (C) Alex Ott
+;;
+;; Author: Alex Ott <alexott@gmail.com>
+;; Keywords:
+;; Requirements:
+;; Status: not intended to be distributed yet
+
+;; common settings for different text & programming modes
+(defun alexott/common-hook ()
+  (local-set-key [return] 'newline-and-indent)
+  (local-set-key "\C-m" 'newline-and-indent)
+  (local-set-key "\C-c:" 'uncomment-region)
+  (local-set-key "\C-c;" 'comment-region)
+  (local-set-key "\C-c\C-c" 'comment-region)
+  )
+
+;; show FIXME/TODO/BUG keywords
+(defun alexott/show-prog-keywords ()
+  ;; highlight additional keywords
+  (font-lock-add-keywords nil '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
+  ;; highlight too long lines
+  (font-lock-add-keywords nil '(("^[^\n]\\{100\\}\\(.*\\)$" 1 font-lock-warning-face t))))
+
+
+;; clean trailing whitespaces automatically
+(setq alexott/trailing-whitespace-modes '(c++-mode c-mode haskell-mode emacs-lisp-mode
+                                                   lisp-mode scheme-mode erlang-mode))
+(defun alexott/trailing-whitespace-hook ()
+  (when (member major-mode alexott/trailing-whitespace-modes)
+    (delete-trailing-whitespace)))
+(add-hook 'before-save-hook 'alexott/trailing-whitespace-hook)
+
+;; untabify some modes
+(setq alexott/untabify-modes '(haskell-mode emacs-lisp-mode lisp-mode scheme-mode erlang-mode))
+(defun alexott/untabify-hook ()
+  (when (member major-mode alexott/untabify-modes)
+    (untabify (point-min) (point-max))))
+(add-hook 'before-save-hook 'alexott/untabify-hook)
+
+
+;;; emacs-rc-common-hooks.el ends here
